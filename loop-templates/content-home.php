06f817b1b883073902497c996a8f0f28fe97bb0b
woodwardtw <twwoodward@me.com>
2022-10-31 13:45:53 -0400

    changing the home page a lot

--- loop-templates/content-home.php
+++ loop-templates/content-home.php
@@ -9,77 +9,25 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class('row'); ?> id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
-
-		 <div class="title-block">
-		    <div class="midd">MI</br>DD</div>
-		    <div class="create">Create</div>
-		  </div>
+	<header class="entry-header col-md-4">
+		<a href="">
+			<img src="<?php echo get_template_directory_uri();?>/imgs/full-logo.svg" class="img-fluid" alt="Midd Create logo.">
+		</a>		
 
 	</header><!-- .entry-header -->
 
 
-	<div class="entry-content">
+	<div class="entry-content col-md-8">
 		<div class="do-block">
-		  <div class="you-can"> A place on the Internet where you can <span
+		  <div class="you-can"> A place on the Internet where you have <span
 		     class="txt-rotate" id="verb"
 		     data-period="2000"
-		     data-rotate='["Make",
-		    "Own",
-		    "Customize",
-		    "Expand",
-		    "Love",
-		    "show",
-		    "shape",
-		    "enjoy",
-		    "elevate",
-		    "prove",
-		    "change",
-		    "tweak",
-		    "grow",
-		    "curate",
-		    "learn",
-		    "do",
-		    "compile",
-		    "communicate",
-		    "demonstrate",
-		    "resist" ]'></span> <span class="period">I</span><span class="period">T</span>!</div>
+		     data-rotate='["control","choice","power"]'></span>.</div>
 		  </div>
 		</div>
-			<!-- <div class="summary">A place on the Internet where you have 
-				<div class="trifecta"><a href="#" class="emph">power</a>, <a href="#" class="emph">choice</a>, & <a href="#" class="emph">control</a>!
-				</div>
-			</div> -->
-		<div class="row">
-			<?php middcreate_homepage_blocks();?>
-		</div>
-		<div class="row goals" id="goal-row">
-			<div class="col-md-4 goal">
-				<h2>Portfolios</h2>
-				<div class="description">
-					Show people who you are.
-				</div>
-				<?php middcreate_creations('portfolio');?>
-			</div>
-			<div class="col-md-4 goal">
-				<h2>Projects</h2>
-				<div class="description">
-					Make something special.
-				</div>
-				<?php middcreate_creations('project');?>	
-			</div>
-			<div class="col-md-4 goal">
-				<h2>Courses</h2>
-				<div class="description">
-					Create a different kind of course.
-				</div>
-				<?php middcreate_creations('course');?>
-			</div>
-		</div>
-		<?php the_content(); ?>
-
+			
 		<?php
 		wp_link_pages(
 			array(
